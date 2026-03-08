736ab7fb8eb66583aa0aaf0750b06a90f0bf7406
ngigi1397-jpg <ngigi1397@gmail.com>
2026-03-08 21:53:46 +0300

    fix: fill empty page files

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -86,10 +86,8 @@ function LoginForm() {
                   onChange={e => setPassword(e.target.value)}
                   onKeyDown={e => e.key === "Enter" && handleLogin()}
                 />
-                <div style={{ textAlign: "right", marginTop: "-0.5rem", marginBottom: "0.8rem" }}>
-                  <a href="/auth/forgot-password" style={{ fontSize: "0.78rem", color: "#c84b31", fontWeight: 500 }}>
-                    Forgot password?
-                  </a>
+                <div className="fg-footer">
+                  <a href="/auth/forgot-password" className="forgot-link">Forgot password?</a>
                 </div>
               </div>
 
